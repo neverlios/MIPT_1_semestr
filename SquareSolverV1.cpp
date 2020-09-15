db631823b507e2caec1ab41d61dc04cba14b49a1
neverlios <70288875+neverlios@users.noreply.github.com>
2020-09-15 17:21:16 +0300

    Add files via upload

---
+++ SquareSolverV1.cpp
@@ -0,0 +1,18 @@
+﻿"Project2.exe" (Win32). Загружено "C:\Users\артём\source\repos\Project2\Debug\Project2.exe". Символы загружены.
+"Project2.exe" (Win32). Загружено "C:\Windows\SysWOW64\ntdll.dll". 
+"Project2.exe" (Win32). Загружено "C:\Windows\SysWOW64\kernel32.dll". 
+"Project2.exe" (Win32). Загружено "C:\Windows\SysWOW64\KernelBase.dll". 
+"Project2.exe" (Win32). Загружено "C:\Windows\SysWOW64\vcruntime140d.dll". 
+"Project2.exe" (Win32). Загружено "C:\Windows\SysWOW64\ucrtbased.dll". 
+Поток 0x1b84 завершился с кодом 0 (0x0).
+"Project2.exe" (Win32). Загружено "C:\Windows\SysWOW64\kernel.appcore.dll". 
+"Project2.exe" (Win32). Загружено "C:\Windows\SysWOW64\msvcrt.dll". 
+"Project2.exe" (Win32). Загружено "C:\Windows\SysWOW64\rpcrt4.dll". 
+"Project2.exe" (Win32). Загружено "C:\Windows\SysWOW64\sspicli.dll". 
+"Project2.exe" (Win32). Загружено "C:\Windows\SysWOW64\cryptbase.dll". 
+"Project2.exe" (Win32). Загружено "C:\Windows\SysWOW64\bcryptprimitives.dll". 
+"Project2.exe" (Win32). Загружено "C:\Windows\SysWOW64\sechost.dll". 
+Поток 0x3ecc завершился с кодом 0 (0x0).
+Поток 0x2b7c завершился с кодом 0 (0x0).
+Поток 0xc40 завершился с кодом 0 (0x0).
+Программа "[1144] Project2.exe" завершилась с кодом 0 (0x0).
